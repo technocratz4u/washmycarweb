a8790e223b21796d27a69a27a6356b1c3aec8952
rahul aggarwal <rahul.a.aggarwal@ericsson.com>
2017-04-14 17:43:53 +0530

    Adding Basic Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { routing } from "./app.routing";
+import { MainbodyComponent } from './mainbody/mainbody.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServicesComponent,
+    MainbodyComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+const APP_ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'aboutus', component: AboutusComponent},
+  {path: 'contactus', component: ContactusComponent}
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
