b8c92119aa1fc26ff05d68d3eda47d705f873050
eaggara <rahul.a.aggarwal@ericsson.com>
2017-04-19 13:52:00 +0530

    commit contactus

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-contactus',
@@ -10,6 +11,26 @@ export class ContactusComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+            var mapOptions = {
+            zoom: 15,
+            center: new google.maps.LatLng(28.67426, 77.31916),
+            mapTypeId: google.maps.MapTypeId.ROAD,
+            scrollwheel: false
+        }
+        var map = new google.maps.Map(document.getElementById('googleMap'),
+            mapOptions);
+
+        var myLatLng = new google.maps.LatLng(28.67426, 77.31916);
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title:"B-23, DSIDC Compound, Jhilmil Industrial Area, Shahdara, Delhi - 110095, India"
+
+        });
+
+
+
   }
 
 }
