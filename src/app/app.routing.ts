da9e2892f0276ee0c345ef41db9d6f56a07317e5
esonray <sonia.raychaudhuri@ericsson.com>
2017-04-19 16:49:12 +0530

    Login designing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,15 +5,13 @@ import { PricingComponent } from './pricing/pricing.component';
 import { HomeComponent } from './home/home.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
-import { LoginComponent } from './login/login.component';
 
 const APP_ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'services', component: ServicesComponent},
   {path: 'pricing', component: PricingComponent},
   {path: 'aboutus', component: AboutusComponent},
-  {path: 'contactus', component: ContactusComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'contactus', component: ContactusComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
